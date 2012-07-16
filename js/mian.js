e9ed0f667440579ef13b7ec60edeec574a5efcb5
psmolenski <pawluch4@gmail.com>
2012-07-16 21:26:12 +0200

    NumberPicker added

--- js/mian.js
+++ js/mian.js
@@ -1,10 +1,75 @@
 Board = function() {
 
+	NumberPicker = function()
+	{
+		this.attached = null;
+		this.numberPicker = null;
+		
+		this.prepare = function(){
+			this.build();
+		}
+		
+		this.build = function(){
+			this.numberPicker = document.createElement('table');
+			this.numberPicker.className = 'number-picker';
+			tbody = document.createElement('tbody');
+			
+			for(rowNum = 0, fieldNum = 1; rowNum < 3; rowNum++)
+			{
+				col = document.createElement('tr');
+				col.className = 'col';	
+				
+				for(colNum = 0; colNum < 3; colNum++)
+				{
+					field = document.createElement('td');
+					field.className = 'field';
+					field.innerHTML = fieldNum++;
+					
+					col.appendChild(field);
+				}
+				
+				tbody.appendChild(col);
+			}
+			this.numberPicker.appendChild(tbody);
+		}
+		
+		this.attach = function(parent){
+			if(this.isAttached()){
+				return;
+			}
+			
+			$(parent).append(this.numberPicker);
+			
+			this.attached = true;
+		}
+		
+		this.detach = function(){
+			if(!this.isAttached){
+				return;
+			}
+			
+			$(this.numberPicker).detach();
+			
+			this.attached = false;
+		}
+		
+		this.isAttached = function()
+		{
+			return this.attached === true;
+		}
+		
+		this.prepare();
+	}
+	
+	this.numberPicker = new NumberPicker();
 	
 	this.prepareBoard = function()
 	{
 		this.build();
 		this.resize();
+		
+		this.numberPicker.attach('body');
+		
 	}
 	
 	this.build = function(){
@@ -39,7 +104,6 @@ Board = function() {
 		
 	}
 	
-
 	this.prepareBoard();
 	
 	
@@ -49,6 +113,17 @@ $(function(){
 	
 	$(window).resize(function(){
 		board.resize();
+	});
+	
+	$('body').on('click', function(){
+		if(board.numberPicker.isAttached())
+		{
+			board.numberPicker.detach();
+		}
+		else
+		{
+			board.numberPicker.attach('body');
+		}
 	});
 	
 });
